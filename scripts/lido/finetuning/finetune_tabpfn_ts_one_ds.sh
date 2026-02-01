37d62352795f1838521db0e2408ed1f78cadd7bf
Friedrich Romberg <frimarom@yahoo.de>
2026-02-01 15:01:20 +0100

    fix oom error

--- scripts/lido/finetuning/finetune_tabpfn_ts_one_ds.sh
+++ scripts/lido/finetuning/finetune_tabpfn_ts_one_ds.sh
@@ -5,6 +5,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --time=02:00:00
+#SBATCH --mem-per-cpu=4G
 #SBATCH --job-name=tabpfn-local-eval
 #SBATCH --output=/work/smfrromb/sbatch_log/tabpfn.%j.out
 #SBATCH --error=/work/smfrromb/sbatch_log/tabpfn.%j.err
