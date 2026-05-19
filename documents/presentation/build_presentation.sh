6645de07c64bb5be7cd236432a9edcef369c05c5
Friedrich Romberg <frimarom@yahoo.de>
2026-05-19 14:09:28 +0200

    began with thesis presentation

---
+++ documents/presentation/build_presentation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+OUT=out
+mkdir -p "$OUT"
+
+pdflatex -output-directory="$OUT" slides.tex
+pdflatex -output-directory="$OUT" slides.tex
+pdflatex -output-directory="$OUT" slides.tex
