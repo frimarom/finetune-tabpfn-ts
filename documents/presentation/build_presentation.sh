2e4715bfb12dc82ef3bae1a04de41784ae4b4fb4
Friedrich Romberg <frimarom@yahoo.de>
2026-05-26 21:37:13 +0200

    finished the presentation

--- documents/presentation/build_presentation.sh
+++ documents/presentation/build_presentation.sh
@@ -6,4 +6,6 @@ OUT=out
 mkdir -p "$OUT"
 
 pdflatex -output-directory="$OUT" slides.tex
+bibtex "$OUT"/slides
+pdflatex -output-directory="$OUT" slides.tex
 pdflatex -output-directory="$OUT" slides.tex
